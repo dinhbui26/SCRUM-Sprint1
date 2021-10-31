33cf04acc609acaf04bde8bac59fec7ef2ee6d6e
Nhan <hieunhan3535@gmail.com>
2021-10-31 07:22:05 +0200

    create project

---
+++ SCRUM2.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { View, Text, Button } from 'react-native';
+
+class Hello extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sprint: 1,
+      member: 3
+    };
+  }
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ margin: 50 }}>Hello world!</Text>
+        <Text style={{ fontWeight: 'bold', fontStyle: 'italic' }}>This is {this.props.name}</Text>
+        <Text style={{ color: 'red' }}>I'm {this.state.age} years old </Text>
+        <Text style={{ backgroundColor: 'cyan' }}>and we have is {this.state.weight} kg</Text>
+        <View style={{ margin: 50 }}>
+          <Button title="NEXT YEAR" onPress={() => this.onPressNextYear()} />
+        </View>
+      </View>
+    );
+  }
+  onPressNextYear() {
+    var curSpr = this.state.sprint;
+    var curMem = this.state.member
+    this.setState({
+      age: curSpr + 1,
+      weight: curMem + 2
+    });
+  }
+}
+export default Hello;
